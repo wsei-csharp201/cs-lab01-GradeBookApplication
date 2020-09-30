33c1f7ab93c4cd4eedbcce4a6d2867fb2873c0e8
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2020-09-30 19:06:52 +0200

    Initial commit

---
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -0,0 +1,79 @@
+﻿using GradeBook.GradeBooks;
+using System;
+
+namespace GradeBook.UserInterfaces
+{
+    public static class StartingUserInterface
+    {
+        public static bool Quit = false;
+        public static void CommandLoop()
+        {
+            while (!Quit)
+            {
+                Console.WriteLine(string.Empty);
+                Console.WriteLine(">> What would you like to do?");
+                var command = Console.ReadLine().ToLower();
+                CommandRoute(command);
+            }
+        }
+
+        public static void CommandRoute(string command)
+        {
+            if (command.StartsWith("create"))
+                CreateCommand(command);
+            else if (command.StartsWith("load"))
+                LoadCommand(command);
+            else if (command == "help")
+                HelpCommand();
+            else if (command == "quit")
+                Quit = true;
+            else
+                Console.WriteLine("{0} was not recognized, please try again.", command);
+        }
+
+        public static void CreateCommand(string command)
+        {
+            var parts = command.Split(' ');
+            if (parts.Length != 2)
+            {
+                Console.WriteLine("Command not valid, Create requires a name.");
+                return;
+            }
+            var name = parts[1];
+            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            Console.WriteLine("Created gradebook {0}.", name);
+            GradeBookUserInterface.CommandLoop(gradeBook);
+        }
+
+        public static void LoadCommand(string command)
+        {
+            var parts = command.Split(' ');
+            if (parts.Length != 2)
+            {
+                Console.WriteLine("Command not valid, Load requires a name.");
+                return;
+            }
+            var name = parts[1];
+            var gradeBook = BaseGradeBook.Load(name);
+
+            if (gradeBook == null)
+                return;
+
+            GradeBookUserInterface.CommandLoop(gradeBook);
+        }
+
+        public static void HelpCommand()
+        {
+            Console.WriteLine();
+            Console.WriteLine("GradeBook accepts the following commands:");
+            Console.WriteLine();
+            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine();
+            Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
+            Console.WriteLine();
+            Console.WriteLine("Help - Displays all accepted commands.");
+            Console.WriteLine();
+            Console.WriteLine("Quit - Exits the application");
+        }
+    }
+}
