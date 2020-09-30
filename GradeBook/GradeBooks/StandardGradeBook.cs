00f997abce937ed1462dcbd8b8ef8f06c9e0dd63
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2020-09-30 19:48:11 +0200

    Update README.md

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name, bool isWighted) : base(name, isWighted)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
