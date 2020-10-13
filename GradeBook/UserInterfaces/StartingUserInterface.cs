1b1a817232a84912a1060292814157bdac9ddfd3
Woskus <woskus20@gmail.com>
2020-10-13 17:32:15 +0200

    Updated StartingUserInterface's CreateCinnabd method

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,22 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var type = parts[2];
+            BaseGradeBook gradeBook;
+
+            switch (type)
+            {
+                case "standard":
+                    gradeBook = new StandardGradeBook(name);
+                    break;
+                case "ranked":
+                    gradeBook = new RankedGradeBook(name);
+                    break;
+                default:
+                    Console.WriteLine(type + " is not a supported type of gradebook, please try again");
+                    return;
+            }
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
