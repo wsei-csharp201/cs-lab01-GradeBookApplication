5e181a8ffdb79645a82d3c0f8853fb618043a2ef
Wiktor Bielecki <wiktor.bielecki@microsoft.wsei.edu.pl>
2022-03-03 13:00:34 +0100

    lab-1

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
