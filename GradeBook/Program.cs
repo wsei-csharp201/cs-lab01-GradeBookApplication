b8076009473c7e15739cf366d84a3603f0ecdaea
krystian-key <kluczkiewicz1512@wp.pl>
2022-03-19 13:19:49 +0100

    Initial commit, the project is compiling

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -16,6 +16,7 @@ namespace GradeBook
             Console.WriteLine("Thank you for using GradeBook!");
             Console.WriteLine("Have a nice day!");
             Console.Read();
+            //initial commit
         }
     }
 }
