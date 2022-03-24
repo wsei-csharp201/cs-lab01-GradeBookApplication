602a0bd72d6806b7d7d6f35e3f4d38d3f7310bb6
Agnieszka_KK <agnieszka.korczakkus@gmail.com>
2022-03-24 21:49:56 +0100

    Commit1

---
+++ GradeBook/GradeBooks/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook
+    {
+        static void StandardGradeBook()
+        {
+            string name;
+
+        }
+    }
+
+    public class StandardGradeBook : BaseGradeBook 
+    {
+    } 
+}
