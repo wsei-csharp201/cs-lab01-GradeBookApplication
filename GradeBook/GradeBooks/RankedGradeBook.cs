e79791bc6035b4dca22094584ad14febcc6ef6f0
mikord45 <51155742+mikord45@users.noreply.github.com>
2022-03-25 23:03:14 +0100

    RankedGradeBook class done

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name):base(name)
+        {
+            Name = name;
+            Students = new List<Student>();
+            Type = GradeBook.Enums.GradeBookType.Ranked;
+        }
+    }
+}
