877ee8caf64edd9a5b4e4811a67b0147592fb98a
mikord45 <51155742+mikord45@users.noreply.github.com>
2022-03-26 00:02:04 +0100

    CalculateStudentStatistics method overrided

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -55,5 +55,17 @@ namespace GradeBook.GradeBooks
                 base.CalculateStatistics();
             } 
         }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
     }
 }
