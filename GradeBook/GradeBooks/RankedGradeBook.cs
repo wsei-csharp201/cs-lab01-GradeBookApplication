08f58aeaf4c2f344701649034970b41b0915cbeb
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2022-03-27 13:28:01 +0200

    Tworzenie klasy RankedGradeBook, dziedziczącej z BaseGradeBook, oraz próba nadpisania metody GetLetterGrade. (nie przechodzi testu)

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(String name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5) 
+                throw new InvalidOperationException("There is less than 5 students.");
+
+            double rank = Students.Count(student => student.AverageGrade > averageGrade) / Students.Count;
+            Console.WriteLine(rank);
+
+            if (rank < 0.2)
+                return 'A';
+            if (rank >= 0.2 && rank < 0.4)
+                return 'B';
+            if (rank >= 0.4 && rank < 0.6)
+                return 'C';
+            if (rank >= 0.6 && rank < 0.8)
+                return 'D';
+            else
+                return 'F';
+
+        }
+
+    }
+}
