624553fdb20d015df7b31bf57a5c5d5c75e64986
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2022-03-27 13:28:44 +0200

    Nadpisanie metod CalculateStatistics, oraz CalculateStudentStatistics dają one informację gdy studentów jest zbyt mało aby wykonać operacje.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -32,6 +32,20 @@ namespace GradeBook.GradeBooks
                 return 'F';
 
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStatistics();
+        }
 
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            else
+                base.CalculateStudentStatistics(name);
+        }
     }
 }
