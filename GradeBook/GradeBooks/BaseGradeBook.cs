dcc5593a72e4066e9bedd674865f208f05666c75
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2022-03-27 13:44:52 +0200

    Dodanie właściwości IsWeighted do BaseGradeBook

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -16,6 +16,8 @@ namespace GradeBook.GradeBooks
 
         public GradeBookType Type { get; set; }
 
+        public bool IsWeighted { get; set; }
+
         public BaseGradeBook(string name)
         {
             Name = name;
