07c9b25629333db6452b1ec30cbe7e1b6f41b3e4
Kamil Szczypczyk <kamil.szczypczyk@microsoft.wsei.edu.pl>
2022-03-27 13:29:55 +0200

    Modyfukacja metody CreateCommand  w StartingUserInterface, dodanie obsługi pozoostałych typów GradeBook

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,13 +34,21 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            BaseGradeBook gradeBook = null;
+            if (parts[2].Equals("standard"))
+                gradeBook = new BaseGradeBook(name);
+            else if (parts[2].Equals("ranked"))
+                gradeBook = new RankedGradeBook(name);
+            else
+                Console.WriteLine(parts[2] + " is not a supported type of gradebook, please try again");
+             
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
