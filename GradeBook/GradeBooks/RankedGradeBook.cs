b6874cefb602d55a5648d2108ddd5b3112ce6c80
Rafal Necek <rafal.necek@microsoft.wsei.edu.pl>
2022-03-31 19:29:01 +0200

    test5

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -7,6 +9,26 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+            else
+            {
+                if (averageGrade >= 80)
+                    return 'A';
+                else if (averageGrade >= 60)
+                    return 'B';
+                else if (averageGrade >= 30)
+                    return 'C';
+                else if (averageGrade >= 20)
+                    return 'D';
+                else
+                    return 'F';
+            }
         }
     }
 }
