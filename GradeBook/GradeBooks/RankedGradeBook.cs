7f210c7536892c71ddb0c980eb666179ae6f7bbe
Rafal Necek <rafal.necek@microsoft.wsei.edu.pl>
2022-03-31 19:09:05 +0200

    test3

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
