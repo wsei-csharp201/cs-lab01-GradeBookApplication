3d29fef82d5dd9359209ff911bd0aeee201da684
Rafal Necek <rafal.necek@microsoft.wsei.edu.pl>
2022-03-31 19:06:14 +0200

    test2

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,12 @@
+﻿using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
