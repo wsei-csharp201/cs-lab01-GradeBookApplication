ae4325708aa2b1b7b64d05de03be77f8620c0ea3
Rafal Necek <rafal.necek@microsoft.wsei.edu.pl>
2022-03-31 19:31:06 +0200

    test6

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -30,5 +30,15 @@ namespace GradeBook.GradeBooks
                     return 'F';
             }
         }
+        public override void CalculateStatistics()
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                    return;
+            }
+
+            base.CalculateStatistics();
+        }
     }
 }
