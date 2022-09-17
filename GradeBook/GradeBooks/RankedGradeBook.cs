b0363ebbeb224dd4e02ed9c20f8f5acfd234f17d
ExtemtedGold1 <wiktorbielecki31@gmail.com>
2022-09-17 14:32:00 +0200

    Override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,6 +9,35 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+
+        }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade > 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade > 60)
+            {
+                return 'B';
+            }
+            else if (averageGrade > 40)
+            {
+                return 'C';
+            }
+            else if (averageGrade > 20)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+
         }
     }
 }
