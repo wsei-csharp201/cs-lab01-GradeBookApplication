28cbbad8f9e8c573c695c82caf9768d5650ab65b
Jakub Ciecieląg <jakub.ciecielag@microsoft.wsei.edu.pl>
2023-03-03 10:33:09 +0100

    Creating new test branch

---
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
