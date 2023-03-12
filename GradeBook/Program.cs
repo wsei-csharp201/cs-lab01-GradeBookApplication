340e74e09b6bb0ffb63f975552a4d601308d109c
Piotr Grzegorzewski <kwaadrat@MacBook-Pro-Piotr.local>
2023-03-12 11:27:30 +0100

    „TESTED”

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -8,7 +8,7 @@ namespace GradeBook
         static void Main(string[] args)
         {
             Console.WriteLine("#=======================#");
-            Console.WriteLine("# Welcome to GradeBookTEST! #");
+            Console.WriteLine("# Welcome to GradeBook! #");
             Console.WriteLine("#=======================#");
 
             StartingUserInterface.CommandLoop();
