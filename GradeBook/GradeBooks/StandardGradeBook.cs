e281e302b51c75956b6e1bb5013db053b3ee92a0
Aleks <aleks.koloch@gmail.com>
2023-03-13 17:10:02 +0100

    Create a class StandardGradeBook

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+
+    }
+}
