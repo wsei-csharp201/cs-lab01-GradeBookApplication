e99ba2cef15da25cf1635d921a1dbefe9400ac1f
Aleks <aleks.koloch@gmail.com>
2023-03-13 17:12:26 +0100

    Create a constructor for StandardGradeBook

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -8,6 +8,9 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Type = Enums.GradeBookType.Standard;
+        }
     }
 }
