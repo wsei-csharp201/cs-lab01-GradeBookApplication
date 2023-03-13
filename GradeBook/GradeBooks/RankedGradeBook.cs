59c014f4c9c1f18c7d6a1df8722c499bcef88735
Wojciech Surmiak <assasyndylan@gmail.com>
2023-03-14 00:58:56 +0100

    wykonane 13/24

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using GradeBook.Enums;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -47,6 +48,26 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException();
             }
+            
         }
+        public override void CalculateStatistics()
+        {
+            if(Students.Count<5)
+            {
+                Console.WriteLine($"Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine($"Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
+
     }
 }
