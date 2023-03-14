0816e0ea1a18dd27aecb706182fedd0b9816049b
Wojciech Surmiak <assasyndylan@gmail.com>
2023-03-14 12:46:51 +0100

    24/24

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -110,20 +110,26 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            double Grade = 0;
+            if((studentType==StudentType.Honors|| studentType==StudentType.DualEnrolled) && IsWeighted == true)
+            {
+                Grade += 1;
+            }
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return Grade + 4;
                 case 'B':
-                    return 3;
+                    return Grade + 3;
                 case 'C':
-                    return 2;
+                    return Grade + 2;
                 case 'D':
-                    return 1;
+                    return Grade + 1;
                 case 'F':
-                    return 0;
+                    return Grade + 0;
             }
-            return 0;
+            return Grade;
         }
 
         public virtual void CalculateStatistics()
