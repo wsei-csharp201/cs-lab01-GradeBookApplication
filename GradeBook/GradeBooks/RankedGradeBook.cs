e63cd35ad6348f919a60a144689250f53b74ae19
jciecielag <jakubciecielag@o2.pl>
2023-03-22 14:40:28 +0100

    Poprawne zaliczenie grupy testów "CreateStandardGradeBookandRankedGradeBookClassesTests"

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    internal class RankedGradeBook : BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -8,7 +8,8 @@ namespace GradeBook.GradeBooks
     {
         public StandardGradeBook(string name) : base(name)
         {
-            Type = Enums.GradeBookType.Standard;
+            
+            Type = Enums.GradeBookType.Standard; 
         }
     }
 }
