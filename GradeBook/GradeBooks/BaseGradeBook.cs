3abcd487b9b56fa77bac3f3219140bd322282875
Pateusz <Pateusz@DESKTOP-682AFMG>
2023-03-30 20:03:42 +0200

    1 punkt

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -10,7 +10,8 @@ using Newtonsoft.Json.Linq;
 namespace GradeBook.GradeBooks
 {
     public class BaseGradeBook
-    {
+    {   
+        public GradeBookType Type { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
 
