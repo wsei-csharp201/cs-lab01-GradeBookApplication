fce4b52ead74f84c0aef0c5e85ea1a70dc68d198
Michał Szura <115370163+Malamity@users.noreply.github.com>
2023-03-30 10:25:09 +0200

    StandardGradeBook

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name, bool IsWeighted) : base(name, IsWeighted)
+        {
+            Type = GradeBookType.Standard;
+        }
+    }
+}
