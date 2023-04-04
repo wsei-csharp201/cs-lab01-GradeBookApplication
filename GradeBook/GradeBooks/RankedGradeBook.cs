d840711151634ca6b62c6dd55c0f41cc8c76ff1c
Pateusz <Pateusz@DESKTOP-682AFMG>
2023-04-05 00:22:34 +0200

    override v2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,53 +1,72 @@
-﻿using System;
+﻿using GradeBook.Enums;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
 
-        public RankedGradeBook(string name, bool IsWeight) : base(name,IsWeight)
+        public RankedGradeBook(string name, bool IsWeight) : base(name, IsWeight)
         {
             this.Type = Enums.GradeBookType.Ranked;
         }
 
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count < 5)
+            try
+            {
+                if (Students.Count < 5)
+                {
+                    throw new InvalidOperationException();
+                }
+                if (averageGrade <= 20)
+                {
+                    return 'F';
+                }
+                else if (averageGrade > 20 && averageGrade <= 40)
+                {
+                    return 'D';
+                }
+                else if (averageGrade > 40 && averageGrade <= 60)
+                {
+                    return 'C';
+                }
+                else if (averageGrade > 60 && averageGrade <= 80)
+                {
+                    return 'B';
+                }
+                else
+                {
+                    return 'A';
+                }
+            }
+            catch (Exception c)
             {
                 throw new InvalidOperationException();
             }
-            if (averageGrade <= 20)
-            {
-                return 'F';
-            }
-            else if (averageGrade > 20 && averageGrade <= 40)
-            {
-                return 'D';
-            }
-            else if (averageGrade > 40 && averageGrade <= 60)
-            {
-                return 'C';
-            }
-            else if (averageGrade > 60 && averageGrade <= 80)
-            {
-                return 'B';
-            }
-            else
-            {
-                return 'A';
-            }
         }
         public override void CalculateStatistics()
-        { 
-          if(Students.Count < 5)
+        {
+            if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students.");
+                Console.WriteLine($"Ranked grading requires at least 5 students.");
+                return;
             }
-            else { 
             base.CalculateStatistics();
-                 } 
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine($"Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
         }
     }
 }
