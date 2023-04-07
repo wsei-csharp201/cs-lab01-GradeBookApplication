0e16a28a8b4fdefea24a5bd1655faad42bce746a
Dawideq9 <127683895+Dawideq9@users.noreply.github.com>
2023-04-07 21:23:07 +0200

    ranked and standard gradebook classes

---
+++ RankedGradeBook.cs
@@ -0,0 +1,13 @@
+﻿
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
