0121c2722a74b0bb1d14290d39aab06b4ecd51d5
Dawideq9 <127683895+Dawideq9@users.noreply.github.com>
2023-04-07 21:57:19 +0200

    overriding rankedgradebook methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,5 +9,45 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new System.InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                System.Console.WriteLine("Ranked grading requires at least 5 students");
+                return;
+            }
+
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                System.Console.WriteLine("Ranked grading requires at least 5 students");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
